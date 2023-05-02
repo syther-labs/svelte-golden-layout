ad34900c4c0357f85889c35d9ac435b426507099
Swoorup Joshi <swoorupj@gmail.com>
2023-05-03 04:58:24 +1000

    CHECKPOINT

--- src/AppStandalone.svelte
+++ src/AppStandalone.svelte
@@ -5,6 +5,8 @@
 		GoldenLayout,
 		RowOrColumnItemConfig,
 		RowOrColumn,
+		ComponentItemConfig,
+		ContentItem,
 	} from 'golden-layout';
 	import 'golden-layout/dist/css/goldenlayout-base.css';
 	import 'golden-layout/dist/css/themes/goldenlayout-dark-theme.css';
@@ -12,6 +14,7 @@
 
 	let container: HTMLDivElement;
 	let myLayout: GoldenLayout;
+	let id = 0;
 
 	onMount(() => {
 		const config: LayoutConfig = {
@@ -19,25 +22,19 @@
 				type: 'row',
 				content: [
 					{
-						type: 'component',
-						componentType: 'myComponent',
-						componentState: { id: 1 },
+						...newElConfig(),
 					},
 					{
 						type: 'column',
 						content: [
 							{
-								type: 'component',
-								componentType: 'myComponent',
-								componentState: { id: 1 },
+								...newElConfig(),
 							},
 							{
 								type: 'row',
 								content: [
 									{
-										type: 'component',
-										componentType: 'myComponent',
-										componentState: { id: 1 },
+										...newElConfig(),
 									},
 								],
 							},
@@ -57,6 +54,51 @@
 		myLayout.loadLayout(config);
 	});
 
+	function newElConfig(): ComponentItemConfig {
+		const _id = id++;
+		return {
+			title: String(_id),
+			type: 'component',
+			componentType: 'myComponent',
+			componentState: { id: _id },
+		};
+	}
+
+	function findClosestParent(
+		predicate: (item: ContentItem) => boolean,
+		start: ContentItem,
+	): ContentItem | undefined {
+		let current = start;
+		while (current) {
+			if (predicate(current)) return current;
+			else current = current.parent;
+		}
+	}
+
+	function findClosestChildren(
+		predicate: (index: number, item: ContentItem) => boolean,
+		start: ContentItem = myLayout.rootItem,
+	): ContentItem | undefined {
+		// First iterate through all children from last to first
+		for (let i = start.contentItems.length - 1; i >= 0; i--) {
+			const child = start.contentItems[i];
+			if (predicate(i, child)) {
+				return child;
+			}
+		}
+
+		// If not matched, then dive deeper from last to first
+		for (let i = start.contentItems.length - 1; i >= 0; i--) {
+			const child = start.contentItems[i];
+			const result = findClosestChildren(predicate, child);
+			if (result) {
+				return result;
+			}
+		}
+
+		return undefined;
+	}
+
 	let isVSplit = true;
 	function addComponent() {
 		const ty = isVSplit ? 'row' : 'column';
@@ -66,18 +108,21 @@
 			type: ty,
 			content: [
 				{
-					type: 'component',
-					componentType: 'myComponent',
-					componentState: { id: Math.floor(Math.random() * 1000) },
+					...newElConfig(),
 				},
 			],
 		};
 
 		const typeId = isVSplit ? 4 : 5;
 
-    let a = myLayout.focusedComponentItem;
-    (a.parent.parent as RowOrColumn).addItem(newRowConfig);
-    myLayout.focusComponent()
+    // find the point from which to add
+    let startNode = myLayout.rootItem;
+    if (myLayout.focusedComponentItem) 
+      startNode = findClosestParent(i => i.isRow || i.isColumn, myLayout.focusedComponentItem) ?? myLayout.rootItem;
+
+		(startNode as RowOrColumn).addItem(newRowConfig);
+
+		// myLayout.focusComponent();
 		// myLayout.addItemAtLocation(newRowConfig, [
 		// (myLayout.rootItem as RowOrColumn).addItem(newRowConfig);
 

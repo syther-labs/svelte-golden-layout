2ad1831dda9e152a3034f1689f3832e681a9b878
Swoorup Joshi <swoorupj@gmail.com>
2023-05-02 22:08:18 +1000

    test

--- src/routes/XAxis.svelte
+++ src/routes/XAxis.svelte
@@ -1,8 +1,12 @@
 <script>
-  export let id;
+	export let id;
+  let w;
+  let h;
 </script>
 
-<div>
-  <h3>X-axis Component for Component {id}</h3>
-  <!-- Your X-axis component content goes here -->
+<p>size: {w}px x {h}px</p>
+
+<div bind:clientWidth={w} bind:clientHeight={h}>
+	<h3>X-axis Component for Component {id}</h3>
+	<!-- Your X-axis component content goes here -->
 </div>
